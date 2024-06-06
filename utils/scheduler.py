5550cbf6d84ef261a7b1b5d077bc24d6da6c0ef5
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-06-07 00:18:21 +0100

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,12 +1,14 @@
 import asyncio
+import logging
 
 async def reboot_bot(bot):
+    logging.info("Rebooting bot")
     await bot.close()
 
 def schedule_reboot(bot):
     async def reboot_task():
         while True:
-            await asyncio.sleep(1800)  # 5 hours
+            await asyncio.sleep(18000)  # 5 hours
             await reboot_bot(bot)
 
     bot.loop.create_task(reboot_task())
