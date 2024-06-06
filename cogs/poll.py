c5ed5432ae6f8076f7bdff5694cb4b2574010040
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-06-06 22:15:57 +0100

    Update poll.py

--- cogs/poll.py
+++ cogs/poll.py
@@ -7,10 +7,10 @@ class Poll(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="poll", description="Create a poll")
-    async def poll(self, interaction: discord.Interaction, question: str, *options: str):
+    async def poll(self, interaction: discord.Interaction, question: str, option1: str, option2: str):
         embed = discord.Embed(title="Poll", description=question, color=0x00ff00)
-        for i, option in enumerate(options, 1):
-            embed.add_field(name=f'Option {i}', value=option, inline=False)
+        embed.add_field(name="Option 1", value=option1, inline=False)
+        embed.add_field(name="Option 2", value=option2, inline=False)
         await interaction.response.send_message(embed=embed)
 
     @app_commands.command(name="pm", description="Send a private message")
