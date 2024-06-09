d6ae8f1c790d8866492c3e2acbf251f8bc6a00bb
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-06-09 22:10:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,10 @@ from discord.ext import commands
 import os
 import asyncio
 import logging
-import sys
 
-# Setup logging
 logging.basicConfig(level=logging.INFO)
 
-# Load environment variables
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-
-intents = discord.Intents.default()
-intents.members = True
-intents.voice_states = True
-intents.message_content = True
-
-# Initialize bot with commands.Bot
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 async def close_sessions():
     await bot.session.close()
@@ -42,20 +31,6 @@ async def on_close():
     logging.info("Bot is closing")
     await close_sessions()
 
-async def reboot_bot():
-    logging.info("Rebooting bot")
-    await bot.close()
-    os.execl(sys.executable, sys.executable, *sys.argv)  # Restart the script
-
-def schedule_reboot(bot):
-    async def reboot_task():
-        while True:
-            await asyncio.sleep(18000)  # 5 hours
-            await reboot_bot()
-
-    bot.loop.create_task(reboot_task())
-
-# Define an asynchronous function to load cogs
 async def load_extensions():
     try:
         await bot.load_extension('cogs.general')
@@ -66,18 +41,19 @@ async def load_extensions():
         await bot.load_extension('cogs.relocate')
         await bot.load_extension('cogs.watermark')
         await bot.load_extension('cogs.serverstats')
-        await bot.load_extension('cogs.talk')  # Add this line
+        await bot.load_extension('cogs.talk')
     except Exception as e:
         logging.exception("Failed to load extensions")
 
-# Define the main function to run the bot
 async def main():
     async with bot:
         await load_extensions()
-        schedule_reboot(bot)
-        await bot.start(DISCORD_BOT_TOKEN)
+        token = os.getenv('DISCORD_BOT_TOKEN')
+        if not token:
+            logging.error("Bot token not found. Please set the DISCORD_BOT_TOKEN environment variable.")
+            return
+        await bot.start(token)
 
-# Run the main function using asyncio.run
 if __name__ == "__main__":
     try:
         asyncio.run(main())
