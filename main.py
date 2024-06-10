de6630bce77db8bc3db0b27796464fd138af5a73
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-06-10 22:03:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ from discord.ext import commands
 import os
 import asyncio
 import logging
+import sys
 
+# Setup logging
 logging.basicConfig(level=logging.INFO)
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+# Load environment variables
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+intents = discord.Intents.default()
+intents.members = True
+intents.voice_states = True
+intents.message_content = True
+
+# Initialize bot with commands.Bot
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def close_sessions():
     await bot.session.close()
@@ -31,6 +42,7 @@ async def on_close():
     logging.info("Bot is closing")
     await close_sessions()
 
+# Define an asynchronous function to load cogs
 async def load_extensions():
     try:
         await bot.load_extension('cogs.general')
@@ -38,22 +50,20 @@ async def load_extensions():
         await bot.load_extension('cogs.poll')
         await bot.load_extension('cogs.admin')
         await bot.load_extension('cogs.voice')
-        await bot.load_extension('cogs.relocate')
         await bot.load_extension('cogs.watermark')
+        await bot.load_extension('cogs.relocate')
         await bot.load_extension('cogs.serverstats')
         await bot.load_extension('cogs.talk')
     except Exception as e:
         logging.exception("Failed to load extensions")
 
+# Define the main function to run the bot
 async def main():
     async with bot:
         await load_extensions()
-        token = os.getenv('DISCORD_BOT_TOKEN')
-        if not token:
-            logging.error("Bot token not found. Please set the DISCORD_BOT_TOKEN environment variable.")
-            return
-        await bot.start(token)
+        await bot.start(DISCORD_BOT_TOKEN)
 
+# Run the main function using asyncio.run
 if __name__ == "__main__":
     try:
         asyncio.run(main())
