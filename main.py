f881bab98ab8560039e375f7aeca82bbcc759e66
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-07-04 21:07:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,9 @@ async def load_extensions():
         await bot.load_extension('cogs.watermark_user')
         await bot.load_extension('cogs.attack')  # Added attack extension
         await bot.load_extension('cogs.new_users')  # Added new_users extension
-        await bot.load_extension('cogs.role') 
+        await bot.load_extension('cogs.role')
+      await bot.load_extension('cogs.boss') 
+
     except Exception as e:
         logging.exception("Failed to load extensions")
 
