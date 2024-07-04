8e991b7051f67760ad223f95d8c700eebf851494
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-07-04 16:36:24 +0100

    Update new_users.py

--- cogs/new_users.py
+++ cogs/new_users.py
@@ -16,9 +16,9 @@ class NewUsers(commands.Cog):
         guild_config = GUILD_IDS.get(member.guild.id)
         if guild_config:
             try:
-                # Send a welcome message to the new user
+                # Send a welcome message to the new user with the server name
                 await member.send(
-                    "Hello and welcome to the server! Please reply to me with your in-game name so I can update your server name and assign your permissions."
+                    f"Hello and welcome to {member.guild.name}! Please reply to me with your in-game name so I can update your server name and assign your permissions."
                 )
             except discord.Forbidden:
                 logging.error(f"Cannot send a DM to {member}. They may have DMs disabled.")
@@ -34,31 +34,38 @@ class NewUsers(commands.Cog):
         if isinstance(message.channel, discord.DMChannel) and message.author != self.bot.user:
             member = message.author
             in_game_name = message.content.strip()
+            current_guild = None
+            other_guild = None
 
-            for guild_id, config in GUILD_IDS.items():
+            for guild_id in GUILD_IDS.keys():
                 guild = self.bot.get_guild(guild_id)
                 if guild and guild.get_member(member.id):
-                    # Identify the appropriate role
-                    target_role = None
-                    for role_name in config['roles']:
-                        role = discord.utils.get(guild.roles, name=role_name)
-                        if role:
-                            target_role = role
-                            break
+                    current_guild = guild
+                else:
+                    other_guild = self.bot.get_guild(guild_id)
 
-                    try:
-                        # Update the user's nickname in the server
-                        await guild.get_member(member.id).edit(nick=in_game_name)
+            if current_guild and other_guild:
+                # Find the role in the other guild
+                config = GUILD_IDS[other_guild.id]
+                target_role = None
+                for role_name in config['roles']:
+                    role = discord.utils.get(other_guild.roles, name=role_name)
+                    if role:
+                        target_role = role
+                        break
 
-                        # Assign the appropriate role to the user
-                        if target_role:
-                            await guild.get_member(member.id).add_roles(target_role)
+                try:
+                    # Update the user's nickname in the other server
+                    await other_guild.get_member(member.id).edit(nick=in_game_name)
 
-                        await member.send(f"Your in-game name has been updated to '{in_game_name}' and you have been assigned the '{target_role.name}' role.")
-                    except Exception as e:
-                        logging.exception(f"Error updating nickname and assigning role: {e}")
-                        await member.send("There was an error updating your nickname or assigning your role. Please contact an admin.")
-                    break
+                    # Assign the appropriate role to the user in the other server
+                    if target_role:
+                        await other_guild.get_member(member.id).add_roles(target_role)
+
+                    await member.send(f"Your in-game name has been updated to '{in_game_name}' and you have been assigned the '{target_role.name}' role in {other_guild.name}.")
+                except Exception as e:
+                    logging.exception(f"Error updating nickname and assigning role: {e}")
+                    await member.send("There was an error updating your nickname or assigning your role. Please contact an admin.")
 
 async def setup(bot):
     cog = NewUsers(bot)
