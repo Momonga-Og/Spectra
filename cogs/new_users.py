8f195ea1bad2eaeb95a74711ecc35b6c0ca14702
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-07-04 19:10:29 +0100

    Update new_users.py

--- cogs/new_users.py
+++ cogs/new_users.py
@@ -3,10 +3,12 @@ from discord.ext import commands
 import logging
 
 GUILD_1_ID = 1218809218342326313
-GUILD_1_CONFIG = {'roles': ['hero', 'member'], 'channel': 'greeting'}
+GUILD_1_ROLE = 'hero'
+GUILD_1_CHANNEL = 'greeting'
 
 GUILD_2_ID = 1258016370314969148
-GUILD_2_CONFIG = {'roles': ['hero', 'member'], 'channel': 'greeting'}
+GUILD_2_ROLE = 'member'
+GUILD_2_CHANNEL = 'greeting'
 
 GUILD_NAMES = {
     GUILD_1_ID: "Server 1",
@@ -20,15 +22,9 @@ class NewUsers(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild_id = member.guild.id
-        guild_config = None
 
-        if guild_id == GUILD_1_ID:
-            guild_config = GUILD_1_CONFIG
-        elif guild_id == GUILD_2_ID:
-            guild_config = GUILD_2_CONFIG
-
-        if guild_config:
-            server_name = GUILD_NAMES.get(guild_id, "the server")
+        if guild_id in GUILD_NAMES:
+            server_name = GUILD_NAMES[guild_id]
             try:
                 # Send a welcome message to the new user
                 await member.send(
@@ -37,7 +33,7 @@ class NewUsers(commands.Cog):
             except discord.Forbidden:
                 logging.error(f"Cannot send a DM to {member}. They may have DMs disabled.")
                 # Notify the greeting channel in the respective server
-                channel = discord.utils.get(member.guild.text_channels, name=guild_config['channel'])
+                channel = discord.utils.get(member.guild.text_channels, name=GUILD_1_CHANNEL if guild_id == GUILD_1_ID else GUILD_2_CHANNEL)
                 if channel:
                     await channel.send(
                         f"Welcome {member.mention}! Please enable DMs so the bot can set your in-game name and permissions. Alternatively, reply here with your in-game name."
@@ -49,27 +45,23 @@ class NewUsers(commands.Cog):
             member = message.author
             in_game_name = message.content.strip()
 
-            for guild_id, config in [(GUILD_1_ID, GUILD_1_CONFIG), (GUILD_2_ID, GUILD_2_CONFIG)]:
+            for guild_id in [GUILD_1_ID, GUILD_2_ID]:
                 guild = self.bot.get_guild(guild_id)
                 guild_member = guild.get_member(member.id) if guild else None
 
                 if guild_member:
-                    target_role = None
-                    for role_name in config['roles']:
-                        role = discord.utils.get(guild.roles, name=role_name)
-                        if role:
-                            target_role = role
-                            break
+                    role_name = GUILD_1_ROLE if guild_id == GUILD_1_ID else GUILD_2_ROLE
+                    role = discord.utils.get(guild.roles, name=role_name)
 
                     try:
                         # Update the user's nickname in the server
                         await guild_member.edit(nick=in_game_name)
                         
                         # Assign the appropriate role to the user
-                        if target_role:
-                            await guild_member.add_roles(target_role)
+                        if role:
+                            await guild_member.add_roles(role)
 
-                        await member.send(f"Your in-game name has been updated to '{in_game_name}' and you have been assigned the '{target_role.name}' role in {GUILD_NAMES[guild_id]}.")
+                        await member.send(f"Your in-game name has been updated to '{in_game_name}' and you have been assigned the '{role_name}' role in {GUILD_NAMES[guild_id]}.")
                     except Exception as e:
                         logging.exception(f"Error updating nickname and assigning role: {e}")
                         await member.send("There was an error updating your nickname or assigning your role. Please contact an admin.")
