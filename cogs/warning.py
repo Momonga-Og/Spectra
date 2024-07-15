27450b957ff4b940b23001a7ccf512fea422e369
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-07-15 08:25:36 +0100

    Create warning.py

---
+++ cogs/warning.py
@@ -0,0 +1,80 @@
+import discord
+from discord.ext import commands, tasks
+from discord import app_commands
+from collections import defaultdict
+import datetime
+
+class WarningSystem(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.action_counts = defaultdict(lambda: defaultdict(int))
+        self.warned_users = set()
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if member.bot:
+            return
+
+        actions = []
+        if before.self_mute != after.self_mute:
+            actions.append("mute/unmute")
+        if before.self_deaf != after.self_deaf:
+            actions.append("deafen/undeafen")
+        if before.channel != after.channel:
+            if before.channel is None:
+                actions.append("join")
+            elif after.channel is None:
+                actions.append("leave")
+            else:
+                actions.append("move")
+
+        for action in actions:
+            self.action_counts[member.id][action] += 1
+            total_actions = sum(self.action_counts[member.id].values())
+
+            if total_actions > 2 and member.id not in self.warned_users:
+                self.warned_users.add(member.id)
+                if after.channel:
+                    await self.warn_user_in_voice(member, after.channel)
+                else:
+                    await self.warn_user_in_text(member)
+
+    async def warn_user_in_voice(self, member, channel):
+        voice_channel = discord.utils.get(member.guild.voice_channels, id=channel.id)
+        if voice_channel:
+            vc = await voice_channel.connect()
+            await self.send_warning(member, vc)
+
+    async def warn_user_in_text(self, member):
+        channel = discord.utils.get(member.guild.text_channels)
+        if channel:
+            await channel.send(
+                f"Please {member.mention}, the functions you are using are not for amusement. They are for practical work, and you are disturbing your fellow users. This is my last warning, or you will be kicked from the server."
+            )
+
+    async def send_warning(self, member, vc):
+        if vc.is_connected():
+            vc.play(discord.FFmpegPCMAudio("warning_message.mp3"))
+            while vc.is_playing():
+                await asyncio.sleep(1)
+            await vc.disconnect()
+        await member.send(
+            f"Please {member.display_name}, the functions you are using are not for amusement. They are for practical work, and you are disturbing your fellow users. This is my last warning, or you will be kicked from the server."
+        )
+
+    @tasks.loop(minutes=1)
+    async def reset_counts(self):
+        now = datetime.datetime.utcnow()
+        for user_id, actions in list(self.action_counts.items()):
+            for action, timestamp in list(actions.items()):
+                if (now - timestamp).total_seconds() > 3600:
+                    del self.action_counts[user_id][action]
+            if not self.action_counts[user_id]:
+                del self.action_counts[user_id]
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.reset_counts.start()
+
+async def setup(bot):
+    await bot.add_cog(WarningSystem(bot))
