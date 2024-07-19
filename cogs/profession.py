8465aacb155b138b6743fc2cfc637e2c728ef5fd
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-07-19 07:52:08 +0100

    Update profession.py

--- cogs/profession.py
+++ cogs/profession.py
@@ -6,7 +6,7 @@ import pandas as pd
 class Profession(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.file_path = 'Professions.xlsx'
+        self.file_path = '/Professions.xlsx'
         self.professions = pd.ExcelFile(self.file_path).sheet_names
 
     @app_commands.command(name="profession", description="Get players by profession")
@@ -21,15 +21,16 @@ class ProfessionView(discord.ui.View):
     def __init__(self, profession_options, file_path):
         super().__init__()
         self.file_path = file_path
-        self.add_item(ProfessionSelect(profession_options))
+        self.add_item(ProfessionSelect(profession_options, file_path))
 
 class ProfessionSelect(discord.ui.Select):
-    def __init__(self, profession_options):
+    def __init__(self, profession_options, file_path):
         super().__init__(placeholder="Choose a profession", min_values=1, max_values=1, options=profession_options)
+        self.file_path = file_path
 
     async def callback(self, interaction: discord.Interaction):
         selected_profession = self.values[0]
-        df = pd.read_excel(self.view.file_path, sheet_name=selected_profession)
+        df = pd.read_excel(self.file_path, sheet_name=selected_profession)
         player_info = df.to_string(index=False)
         await interaction.response.send_message(f"Players with profession {selected_profession}:\n```{player_info}```", ephemeral=True)
 
