bac7fe0b565cb52c99078656e064afe55ccbbffb
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-11-13 05:02:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def on_close():
     await close_sessions()
 
 EXTENSIONS = [
-    'cogs.general', 'cogs.moderation', 'cogs.poll', 'cogs.admin', 'cogs.voice',
+    'cogs.general', 'cogs.moderation', 'cogs.poll', 'cogs.admin',
     'cogs.relocate', 'cogs.watermark', 'cogs.serverstats', 'cogs.talk', 'cogs.write',
     'cogs.watermark_user', 'cogs.attack', 'cogs.new_users', 'cogs.role',
     'cogs.youtube_mp3', 'cogs.image_converter', 'cogs.clear', 'cogs.screenshot',
