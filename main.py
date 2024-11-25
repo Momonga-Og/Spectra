972954e4366eebd8c3f459239cc504802b2a43b3
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-11-25 18:13:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ EXTENSIONS = [
     'cogs.general', 'cogs.moderation', 'cogs.poll', 'cogs.admin', 'cogs.gtoguild', 'cogs.save',
     'cogs.relocate', 'cogs.watermark', 'cogs.serverstats', 'cogs.talk', 'cogs.write',
     'cogs.watermark_user', 'cogs.attack', 'cogs.role', 'cogs.metiers', 'cogs.percopos',
-    'cogs.youtube_mp3', 'cogs.image_converter', 'cogs.clear', 'cogs.startguild',  # Corrected here
+    'cogs.youtube_mp3', 'cogs.image_converter', 'cogs.clear', 'cogs.startguild', 'cogs.percoattack',
     'cogs.rbg', 'cogs.bow', 'cogs.welcomesparta', 'cogs.contract', 'cogs.profession',
     'cogs.super', 'cogs.ai', 'cogs.translator', 'cogs.spotify', 'cogs.voice', 'cogs.youtubemp4',
 ]
