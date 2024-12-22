65766b8c5c2e60a7147ad046f43883f5814b7229
Momonga-Og <162824714+Momonga-Og@users.noreply.github.com>
2024-12-22 08:12:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ import asyncio
 import logging
 import sqlite3
 import sys
-from database import initialize_database
 
-
-initialize_database()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
